0c3c1b44783eca89de96695b6c7f080dd48a4d65
Máté Andrea <74617712+mt-andrea@users.noreply.github.com>
2022-03-13 17:32:52 +0100

    nav WIP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Navbar from './Navbar'
 
 const App = () => {
   return (
-    <>
+    <Router>
         <Navbar/>
-    </>
+    </Router>
   )
 }
 
