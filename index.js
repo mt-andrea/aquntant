8313726b92037aa0871074e1492fe74ebf3b44d7
Borzsikeh <74607362+Borzsikeh@users.noreply.github.com>
2022-03-13 18:34:45 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import bootstrap from 'bootstrap';
+const $ = require("jquery")(window);
+const Chart = require('chart.js');
+
+ReactDOM.render( < App /> ,
+
+    document.getElementById('root')
+);
