77018bd3e332e2e0da0318ca2a6682010be9a079
mt-andrea <mateandicica@gmail.com>
2022-03-17 19:57:25 +0100

    styled succsess page

--- src/components/pages/Successfull-Reg.jsx
+++ src/components/pages/Successfull-Reg.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const SuccessfullReg = () => {
   return (
-    <div>SuccessfullReg</div>
+    <div className='d-flex justify-content-around' style={{height:'100vh',background: '#A8FBFF'}}>
+        <h2 style={{margin:'25vh'}}> Registration successfull! Now You can Sign In.</h2></div>
   )
 }
 
