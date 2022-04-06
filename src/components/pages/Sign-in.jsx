99c47d677c62a93ffa29a6e52f894467dbbe925f
mt-andrea <mateandicica@gmail.com>
2022-04-06 18:08:59 +0200

    sign up page needs TLC

--- src/components/pages/Sign-in.jsx
+++ src/components/pages/Sign-in.jsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import style from '../../const/style'
+import { useNavigate } from 'react-router-dom';
+import { ExclamationCircleFill } from 'react-bootstrap-icons'
 
 const SignIn = () => {
+
+  let navigate = useNavigate();
+
+  const [message, setMessage] = useState("")
+
+  const [data, setData] = useState({
+    username: "",
+    password: ""
+  })
+
+  function change(e) {
+    const { name, value } = e.target
+    setData({
+      ...data,
+      [name]: value
+    })
+  }
+
+  function signin(e) {
+    e.preventDefault()
+    if (data.username == "" || data.password == "") {
+      setMessage("Username or password is empty!")
+      return
+    }
+  }
+
+  const reg = (e) => {
+    e.preventDefault()
+    navigate('/sign-up')
+  }
+
+  const Message = () => {
+    let feltetel = message != ""
+    return (
+      <p>{feltetel && <ExclamationCircleFill />} {message}</p>
+    )
+  }
+
   return (
-    <div>SignIn</div>
+    <div className='d-flex justify-content-center' style={style.content}>
+      <form>
+        <fielset className='m-3' style={{ width: '50vw' }}>
+          <legend className='text-center'>Sign In</legend>
+          <p className='text-center' style={{ color: "red", fontWeight: "bolder" }}>{<Message />}</p>
+          <div className='d-flex flex-md-row justify-content-evenly m-2'>
+            <label className='d-flex flex-column align-items-end w-25' htmlFor="username">Username: </label>
+            <input className='d-flex flex-column form-control w-50' type="text" name="username" id="username" value={data.username} /*onChange={change}*/ />
+          </div>
+          <div className='d-flex flex-md-row justify-content-evenly m-2'>
+            <label className='d-flex flex-column align-items-end w-25' htmlFor="password">Password: </label>
+            <input className='d-flex flex-column form-control w-50' type="password" name="password" id="password" value={data.password} /*onChange={change}*/ />
+          </div>
+        </fielset>
+        <div className='d-flex flex-row justify-content-around'>
+          <button className='form-control btn m-3' style={style.btnPrim} onClick={signin} type="submit">Sign In</button>
+          <button className='form-control btn m-3' style={style.btnSec} onClick={reg}>Register</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
