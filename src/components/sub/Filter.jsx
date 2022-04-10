8d8f4ed2270cd81aca112731e895ba7321eab505
Máté Andrea <74617712+mt-andrea@users.noreply.github.com>
2022-04-10 17:47:56 +0200

    placeholders

--- src/components/sub/Filter.jsx
+++ src/components/sub/Filter.jsx
@@ -2,44 +2,47 @@ import React from 'react'
 import style from '../../const/style'
 
 const Filter = () => {
-  return (
-    <div  style={style.strip_form}>
-        <form>
-            <fieldset className='d-flex flex-md-row justify-content-evenly m-2'>
-                <div className='d-flex flex-row align-items-center'>
-                    <label className='m-2' htmlFor='in_out'>Income/Outcome </label>
-                    <select className='form-control p-1' id='in_out'>
-                        <option value={'-'}>Outcome</option>
-                        <option value={'+'}>Income</option>
-                    </select>
-                </div>
-                <div className='d-flex flex-row align-items-center'>
-                    <label className='m-2' htmlFor='month'>Month</label>
-                    <select className='form-control p-1' id='month'>
-                        <option value={'01'}>January</option>
-                        <option value={'02'}>February</option>
-                        <option value={'03'}>March</option>
-                        <option value={'04'}>April</option>
-                        <option value={'05'}>May</option>
-                        <option value={'06'}>June</option>
-                        <option value={'07'}>July</option>
-                        <option value={'08'}>August</option>
-                        <option value={'09'}>September</option>
-                        <option value={'10'}>October</option>
-                        <option value={'11'}>November</option>
-                        <option value={'12'}>December</option>
-                    </select>
-                </div>
-                <div className='d-flex flex-row align-items-center'>
-                    <label className='m-2' htmlFor='partner'>Partner</label>
-                    <select  className='form-control p-1' id='partner'>
+    return (
+        <div style={style.strip_form}>
+            <form>
+                <fieldset className='d-flex flex-md-row justify-content-evenly m-2'>
+                    <div className='d-flex flex-row align-items-center'>
+                        <label className='m-2' htmlFor='in_out'>Income/Outcome </label>
+                        <select className='form-control p-1' id='in_out'>
+                            <option value={'0'}>-- Select --</option>
+                            <option value={'-'}>Outcome</option>
+                            <option value={'+'}>Income</option>
+                        </select>
+                    </div>
+                    <div className='d-flex flex-row align-items-center'>
+                        <label className='m-2' htmlFor='month'>Month</label>
+                        <select className='form-control p-1' id='month'>
+                            <option value={'00'}>-- Select --</option>
+                            <option value={'01'}>January</option>
+                            <option value={'02'}>February</option>
+                            <option value={'03'}>March</option>
+                            <option value={'04'}>April</option>
+                            <option value={'05'}>May</option>
+                            <option value={'06'}>June</option>
+                            <option value={'07'}>July</option>
+                            <option value={'08'}>August</option>
+                            <option value={'09'}>September</option>
+                            <option value={'10'}>October</option>
+                            <option value={'11'}>November</option>
+                            <option value={'12'}>December</option>
+                        </select>
+                    </div>
+                    <div className='d-flex flex-row align-items-center'>
+                        <label className='m-2' htmlFor='partner'>Partner</label>
+                        <select className='form-control p-1' id='partner'>
+                            <option value={'0'}>-- Select --</option>
 
-                    </select>
-                </div>
-            </fieldset>
-        </form>
-    </div>
-  )
+                        </select>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    )
 }
 
 export default Filter
